dabd1e9a3df98c10a27fb4b3864e33dd634bcd43
Carlos Segura <lithos.contact@gmail.com>
2023-05-08 07:28:08 +0200

    feat(ci-cd): fix a type issue

--- src/components/SpecieChip.vue
+++ src/components/SpecieChip.vue
@@ -16,7 +16,7 @@ import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
 
 const props = defineProps<{
-	specie: 'dog' | 'cat';
+	specie: 'dog' | 'cat' | string;
 }>();
 
 const translatedSpecie = computed(() =>
